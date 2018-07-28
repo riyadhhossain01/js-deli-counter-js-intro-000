9b003c18111f00803570a4bfb4d5880c590a95e4
riyadh hossain <riyadh_h@ymail.com>
2018-07-28 03:16:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeliLine = []
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name);
+  for(var i =0; i<katzDeliLine.length; i++){
+    if(katzDeliLine[i] === name )
+    return "Welcome, " + katzDeliLine[i] + ". You are number " + (i+1) + " in line."
+  }
+}
+
+function nowServing(){
+  if(katzDeliLine.length == null)
+    return "There is nobody waiting to be served!"
+  else {
+    return ("Currently serving: " + katzDeliLine[0]);
+    katzDeliLine.shift();
+  }
+
+}
+
+function currentLine(array){
+  if(katzDeliLine.length = 0)
+    return "The line is currently empty."
+  else {
+    var x =0;
+    var string = "The line is currently: ";
+    while(x<katzDeliLine.length){
+      string += "${x+1}. "  + katzDeliLine[x];
+      x++
+    }
+    return string;
+  }
+}
