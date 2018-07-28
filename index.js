ccb792c64e6741eb1cdf5bc6060db273a43d11a4
riyadh hossain <riyadh_h@ymail.com>
2018-07-28 03:44:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function nowServing(katzDeliLine){
   if(katzDeliLine.length == 0)
     return "There is nobody waiting to be served!"
   else {
-    var string = "Currently serving " + katzDeliLine.shift()
-    return string.substr(0,string.length-1);
+    return ("Currently serving " + katzDeliLine.shift() + ".");
 
   }
 
