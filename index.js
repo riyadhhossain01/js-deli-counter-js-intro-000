2943edcfc00c8862782e137cd82295d8947a0f3b
riyadh hossain <riyadh_h@ymail.com>
2018-07-28 03:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  if(katzDeliLine.length = 0)
+  if(katzDeliLine.length == 0)
     return "The line is currently empty."
   else {
     var x =0;
